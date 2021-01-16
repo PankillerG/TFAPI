73deeaec1c6117d30eaadc460d542ad0d4f2c1d8
Artyom <savossinartem@gmail.com>
2021-01-16 20:13:57 +0300

    corrected topic check

--- connector/test_talk/connector/trade_brokers/main.py
+++ connector/test_talk/connector/trade_brokers/main.py
@@ -7,16 +7,12 @@ TOKEN = '1234'
 client = Client(TOKEN, True)
 
 def controller(message, producer):
-    r = pb.CandlesRequest()
-    r.ParseFromString(message.value)
     topic = message.topic
 
-    response = pb.Candles()
-    response.error = 'popochka' 
-    response.success = False
-    producer.send(Response_channels[topic], value=response)
     if (topic == 'get_candles'):
         try :
+            r = pb.CandlesRequest()
+            r.ParseFromString(message.value)
             answer = client.get_candles(r.figi, r.from_, r.to_, r.interval)
             producer.send(Response_channels[topic], value=answer)
         except Exception as err:
